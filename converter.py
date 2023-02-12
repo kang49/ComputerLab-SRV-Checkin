cd422af961050534234b7b299abce123b1dfc3bd
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-02-12 16:52:56 +0700

    Save form.xlsx to formData

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import datetime
 
 with open('/volume1/web/ComputerLab-SRV-Checkin/form.json', 'rb') as file:
     data = file.read().decode('utf-8')
@@ -9,4 +10,11 @@ with open('/volume1/web/ComputerLab-SRV-Checkin/form.json', 'w', encoding='utf-8
     json.dump(data, file, ensure_ascii=False)
 
 df = pd.DataFrame(data)
+#Save for system
 df.to_excel("/volume1/web/ComputerLab-SRV-Checkin/form.xlsx", index=False)
+
+#Save for user
+current_month = datetime.datetime.now().strftime("%B")
+current_year = datetime.datetime.now().strftime("%Y")
+file_name = f"/volume1/web/ComputerLab-SRV-Checkin/formData/{current_month}_{current_year}.xlsx"
+df.to_excel(file_name, index=False)
